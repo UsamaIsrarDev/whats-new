3f0e5da135ce35b5d564583846371161e74f5c14
UsamaIsrarDev <usamaisrar1190@gmail.com>
2025-10-03 00:02:22 +0500

    Keep learning with game

--- react/app/components/AdminPanal.tsx
+++
@@ -1,9 +0,0 @@
-const AdminPanal = () => {
-  return (
-    <div className="AdminPanal">
-      <h2>AdminPanal</h2>
-    </div>
-  );
-};
-
-export default AdminPanal;
--- react/app/components/MyButton.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const MyButton = ({ onClick, count }: any) => {
-  return <button onClick={onClick}>Clicked {count} times!</button>;
-};
-
-export default MyButton;
